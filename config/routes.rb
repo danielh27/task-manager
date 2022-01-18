b30eb31120ebd436d40f1e56a991b1cda177ba1a
Daniel Herrera <daniel97hh@gmail.com>
2022-01-18 14:47:56 -0500

    pendiente estilo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'tasks', to: 'tasks#create'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#details', as: 'task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#delete'
 end
